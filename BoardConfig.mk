348a9a6a7197b7465e1f830968919eb17195d223
Nick <nickvanbokhorst@hotmail.com>
2016-08-19 09:02:14 +0200

    Remove optimizations and custom TC's

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,19 +29,6 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_lux
 # Kernel
 TARGET_KERNEL_CONFIG := lux_defconfig
 
-# Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
-KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
-
-# Rom Toolchain
-TARGET_GCC_VERSION_EXP := 4.9
-
-# Optimizations
-STRICT_ALIASING := false
-CLANG_O3 := true
-ENABLE_GCCONLY := true
-GRAPHITE_OPTS := false
-
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432	# 32768 * 1024 mmcblk0p31
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432	# 32768 * 1024 mmcblk0p32
