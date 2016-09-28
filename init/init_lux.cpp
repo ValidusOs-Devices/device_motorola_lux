c5c6eb27da1a95138321b681b4934a8670748584
GtrCraft <nickvanbokhorst@hotmail.com>
2016-09-28 08:30:51 +0200

    Fix up init for N
    This reverts commit 4b967d972f42bcd756f02eed2d56fee529f6eff8 & 1f20c32bf9ef54f533a79569dc3f1e0607b8bd42.

---
+++ init/init_lux.cpp
@@ -0,0 +1,129 @@
+/*
+   Copyright (c) 2014, The Linux Foundation. All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "vendor_init.h"
+#include "property_service.h"
+#include "log.h"
+#include "util.h"
+
+
+static void dual_sim(void);
+static void single_sim(void);
+
+void vendor_load_properties()
+{
+    std::string carrier;
+    std::string device;
+    std::string sku;
+    std::string platform;
+    std::string radio;
+
+    platform = property_get("ro.board.platform");
+    if (platform != ANDROID_TARGET)
+        return;
+
+    radio = property_get("ro.boot.radio");
+    sku = property_get("ro.boot.hardware.sku");
+    carrier = property_get("ro.boot.carrier");
+
+    property_set("ro.product.model", sku.c_str());
+
+    if (carrier == "retgb" || carrier == "reteu" || carrier == "retde"
+            || carrier == "vfau") {
+        // These are single SIM XT1562 devices
+        single_sim();
+        property_set("ro.product.device", "lux");
+        property_set("ro.build.description", "lux_reteu-user 5.1.1 LPD23.118-10 15 release-keys");
+        property_set("ro.build.fingerprint", "motorola/lux_reteu/lux:5.1.1/LPD23.118-10/15:user/release-keys");
+        property_set("ro.build.product", "lux");
+        property_set("ro.mot.build.customerid", "reteu");
+        property_set("ro.gsm.data_retry_config", "default_randomization=2000,max_retries=infinite,1000,1000,80000,125000,485000,905000");
+        property_set("persist.radio.mot_ecc_custid", "emea");
+        property_set("persist.radio.mot_ecc_enabled", "1");
+        property_set("persist.radio.process_sups_ind", "0");
+    }
+    else if (sku == "XT1562" || radio == "0x4") {
+        dual_sim();
+        property_set("ro.product.device", "lux_uds");
+        property_set("ro.build.description", "lux_retasia_ds-user 5.1.1 LPD23.118-10 14 release-keys");
+        property_set("ro.build.fingerprint", "motorola/lux_retasia_ds/lux_uds:5.1.1/LPD23.118-10/14:user/release-keys");
+        property_set("ro.build.product", "lux_uds");
+        property_set("ro.mot.build.customerid", "retasiaall");
+        property_set("ro.gsm.data_retry_config", "default_randomization=2000,max_retries=infinite,1000,1000,80000,125000,485000,905000");
+        property_set("persist.radio.mot_ecc_custid", "emea");
+        property_set("persist.radio.mot_ecc_enabled", "1");
+        property_set("persist.radio.process_sups_ind", "0");
+    }
+    else if (carrier == "retbr" || carrier == "retla" || carrier == "tefbr"
+            || carrier == "timbr" || carrier == "retmx") {
+        // These are dual SIM XT1563 devices
+        dual_sim();
+        property_set("ro.product.device", "lux_uds");
+        property_set("ro.build.description", "lux_retla_ds-user 5.1.1 LPD23.118-6.1 2 release-keys");
+        property_set("ro.build.fingerprint", "motorola/lux_retla_ds/lux_uds:5.1.1/LPD23.118-6.1/2:user/release-keys");
+        property_set("ro.build.product", "lux_uds");
+        property_set("ro.mot.build.customerid", "retla");
+        property_set("ro.gsm.data_retry_config", "default_randomization=2000,max_retries=infinite,1000,1000,80000,125000,485000,905000");
+        property_set("persist.radio.mot_ecc_enabled", "1");
+        property_set("persist.radio.process_sups_ind", "1");
+    }
+    else if (sku == "XT1563" || radio == "0x8") {
+        single_sim();
+        property_set("ro.product.device", "lux");
+        property_set("ro.build.description", "lux_retca-user 5.1.1 LPD23.118-10 19 release-keys");
+        property_set("ro.build.fingerprint", "motorola/lux_retca/lux:5.1.1/LPD23.118-10/19:user/release-keys");
+        property_set("ro.build.product", "lux");
+        property_set("ro.mot.build.customerid", "retca");
+        property_set("ro.gsm.data_retry_config", "");
+        property_set("persist.radio.mot_ecc_enabled", "");
+        property_set("persist.radio.process_sups_ind", "1");
+    }
+
+    device = property_get("ro.product.device");
+    INFO("Found radio id: %s setting build properties for %s device\n", radio.c_str(), device.c_str());
+}
+
+static void dual_sim(void)
+{
+    property_set("persist.radio.force_get_pref", "1");
+    property_set("persist.radio.multisim.config", "dsds");
+    property_set("persist.radio.plmn_name_cmp", "1");
+    property_set("ro.telephony.ril.config", "simactivation");
+}
+
+static void single_sim(void)
+{
+    property_set("persist.radio.force_get_pref", "");
+    property_set("persist.radio.multisim.config", "");
+    property_set("persist.radio.plmn_name_cmp", "");
+    property_set("ro.telephony.ril.config", "");
+}
