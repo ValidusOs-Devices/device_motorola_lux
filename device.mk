c2eb77972f3ee5f8812f2aefbbebb1b350fea622
Alberto97 <albertop2197@gmail.com>
2016-11-12 19:46:10 +0100

    lux: Move firmware extraction script to the common tree
    Change-Id: Ibeeaf64d7290680344e701645be8eeb94e416fa1

--- device.mk
+++ device.mk
@@ -29,14 +29,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_florida.xml
 
-# Firmware Extraction
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/extract_firmware.sh:install/bin/extract_firmware.sh
-
-# Firmware Extraction
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/extract_firmware.sh:install/bin/extract_firmware.sh
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
