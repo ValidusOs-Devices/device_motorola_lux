f28c97c45afbb6ae2ff249360c4c3c444cc4ed74
GtrCraft <nickvanbokhorst@hotmail.com>
2016-11-28 16:10:07 +0100

    Validus N Bringup

--- validus.mk
+++ validus.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 $(call inherit-product, device/motorola/lux/full_lux.mk)
-$(call inherit-product, vendor/tesla/config/caf_fw.mk)
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -21,7 +21,7 @@ TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lux
-PRODUCT_NAME := tesla_lux
+PRODUCT_NAME := validus_lux
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := lux
